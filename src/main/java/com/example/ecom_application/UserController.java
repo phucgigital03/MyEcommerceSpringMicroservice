8fedc0e2626c61872b60d79163098c7593477423
phucnguyen <phucnguyendigital2003@gmail.com>
2025-05-09 07:53:34 +0700

    build user API without Spring Boot JPA

---
+++ src/main/java/com/example/ecom_application/UserController.java
@@ -0,0 +1,47 @@
+package com.example.ecom_application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getAllUsers(){
+        return new ResponseEntity<>(userService.fetchAllUsers(),
+                                    HttpStatus.OK);
+        //return ResponseEntity.ok(userService.fetchAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id){
+        Optional<User> user = userService.fetchUser(id);
+        return user.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> createUser(@RequestBody User user){
+        userService.addUser(user);
+        return ResponseEntity.ok("User added successfully");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id,
+                                             @RequestBody User updatedUser){
+        boolean updated = userService.updateUser(id, updatedUser);
+        if (updated)
+            return ResponseEntity.ok("User updated successfully");
+        return ResponseEntity.notFound().build();
+    }
+
+}
---
+++ src/main/java/com/example/ecom_application/UserService.java
@@ -0,0 +1,39 @@
+package com.example.ecom_application;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
+
+    public List<User> fetchAllUsers() {
+        return userList;
+    }
+
+    public void addUser(User user) {
+        user.setId(nextId++);
+        userList.add(user);
+    }
+
+    public Optional<User> fetchUser(Long id) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+    }
+
+    public boolean updateUser(Long id, User updatedUser) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst()
+                .map(existingUser -> {
+                    existingUser.setFirstName(updatedUser.getFirstName());
+                    existingUser.setLastName(updatedUser.getLastName());
+                    return true;
+                }).orElse(false);
+    }
+}
